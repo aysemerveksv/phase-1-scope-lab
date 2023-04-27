e6ef87de77bb8d5ffba35e0c6d976201c560957c
Ayşe Merve Kosova <mervekosova1@hotmail.com>
2023-04-27 22:59:52 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+// Declare global variable
+var customerName = 'bob';
+
+// Write function to modify and return customerName in uppercase
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+// Write function to declare and modify a global variable
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Write function to overwrite the global variable
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare a constant in global scope
+const leastFavoriteCustomer = 'someone';
+
+// Write function to attempt to modify the constant
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'; // This will throw a TypeError because you can't modify a constant
+}
